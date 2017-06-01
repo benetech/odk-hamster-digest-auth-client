d75de35d64285da983fab8264e18410f5542eeb7
Caden Howell <cadenh@benetech.org>
2017-06-01 01:29:54 -0500

    Add debug logging

--- src/main/java/org/benetech/security/client/digest/HttpComponentsClientHttpRequestFactoryDigestAuth.java
+++ src/main/java/org/benetech/security/client/digest/HttpComponentsClientHttpRequestFactoryDigestAuth.java
@@ -3,6 +3,8 @@ package org.benetech.security.client.digest;
 import java.net.URI;
 import java.util.Random;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpHost;
 import org.apache.http.client.AuthCache;
 import org.apache.http.client.protocol.ClientContext;
@@ -12,62 +14,73 @@ import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
- 
+
 /**
  * Factory to create HttpContext client objects that can use Digest Authentication.
- * @see <a href="http://www.baeldung.com/resttemplate-digest-authentication">http://www.baeldung.com/resttemplate-digest-authentication</a>
+ * 
+ * @see <a href=
+ *      "http://www.baeldung.com/resttemplate-digest-authentication">http://www.baeldung.com/resttemplate-digest-authentication</a>
  * @author Eugen Paraschiv <http://www.baeldung.com/author/eugen/>
  *
  */
-public class HttpComponentsClientHttpRequestFactoryDigestAuth 
-  extends HttpComponentsClientHttpRequestFactory {
- 
-    HttpHost host;
-    String realmName;
- 
-    public HttpComponentsClientHttpRequestFactoryDigestAuth() {
-        super();
-    }
- 
-    @Override
-    protected HttpContext createHttpContext(HttpMethod httpMethod, URI uri) {
-        return createHttpContext();
-    }
- 
-    private HttpContext createHttpContext() {
-        // Create AuthCache instance
-        AuthCache authCache = new BasicAuthCache();
-        // Generate DIGEST scheme object, initialize it and add it to the local auth cache
-        DigestScheme digestAuth = new DigestScheme();
-        // If we already know the realm name
-        digestAuth.overrideParamter("realm", realmName);
-        digestAuth.overrideParamter("nonce", Long.toString(new Random().nextLong(), 36));
+public class HttpComponentsClientHttpRequestFactoryDigestAuth
+    extends HttpComponentsClientHttpRequestFactory {
 
-        authCache.put(host, digestAuth);
- 
-        // Add AuthCache to the execution context
-        BasicHttpContext localcontext = new BasicHttpContext();
-        localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);
-        return localcontext;
-    }
+  private static Log logger =
+      LogFactory.getLog(HttpComponentsClientHttpRequestFactoryDigestAuth.class);
 
-    public HttpHost getHost() {
-      return host;
-    }
+  HttpHost host;
+  String realmName;
 
-    public void setHost(HttpHost host) {
-      this.host = host;
-    }
+  public HttpComponentsClientHttpRequestFactoryDigestAuth() {
+    super();
+  }
 
-    public String getRealmName() {
-      return realmName;
-    }
+  @Override
+  protected HttpContext createHttpContext(HttpMethod httpMethod, URI uri) {
+    return createHttpContext();
+  }
 
-    public void setRealmName(String realmName) {
-      this.realmName = realmName;
+  private HttpContext createHttpContext() {
+    // Create AuthCache instance
+    AuthCache authCache = new BasicAuthCache();
+    // Generate DIGEST scheme object, initialize it and add it to the local auth cache
+    DigestScheme digestAuth = new DigestScheme();
+    // If we already know the realm name
+    digestAuth.overrideParamter("realm", realmName);
+    
+    String nonceString = Long.toString(new Random().nextLong(), 36);
+    
+    digestAuth.overrideParamter("nonce", nonceString);
+
+    if (logger.isDebugEnabled()) {
+      logger.debug("DigestAuth params Nonce " + nonceString + " Realm " + realmName);
     }
     
-    
-    
-    
+    authCache.put(host, digestAuth);
+
+    // Add AuthCache to the execution context
+    BasicHttpContext localcontext = new BasicHttpContext();
+    localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);
+    return localcontext;
+  }
+
+  public HttpHost getHost() {
+    return host;
+  }
+
+  public void setHost(HttpHost host) {
+    this.host = host;
+  }
+
+  public String getRealmName() {
+    return realmName;
+  }
+
+  public void setRealmName(String realmName) {
+    this.realmName = realmName;
+  }
+
+
+
 }
