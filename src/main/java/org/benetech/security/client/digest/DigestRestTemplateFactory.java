2ba8c470187a1fde32f23e7d2af84d74e3ec43ab
Caden Howell <cadenh@benetech.org>
2017-06-02 13:45:10 -0500

    Revert " ODK-215 add proxy configuration for debugging"
    This reverts commit f31dec5dda5cbe8fd9b100238f2a05701ce35c3c.

--- src/main/java/org/benetech/security/client/digest/DigestRestTemplateFactory.java
+++ src/main/java/org/benetech/security/client/digest/DigestRestTemplateFactory.java
@@ -18,7 +18,8 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.RestTemplate;
 
 /**
- * Factory to create RestTemplate client objects that can use Digest Authentication.
+ * Factory to create RestTemplate client objects that can use Digest
+ * Authentication.
  * 
  * @see <a href=
  *      "http://www.baeldung.com/resttemplate-digest-authentication">http://www.baeldung.com/resttemplate-digest-authentication</a>
@@ -29,56 +30,36 @@ public class DigestRestTemplateFactory {
 
   private static Log logger = LogFactory.getLog(DigestRestTemplateFactory.class);
 
-  public static RestTemplate getRestTemplate(String hostname, int port, String scheme,
-      String realmName, String username, String password) {
-    return getRestTemplate(hostname, port, scheme, realmName, username, password, null, false);
-  }
   
-  public static RestTemplate getRestTemplate(String hostname, int port, String scheme,
-      String realmName, String username, String password, boolean useAwsFilter) {
-    return getRestTemplate(hostname, port, scheme, realmName, username, password, null, useAwsFilter);
-  }
+	public static RestTemplate getRestTemplate(String hostname, int port, String scheme, String realmName,
+			String username, String password) {
+		HttpHost host = new HttpHost(hostname, port, scheme);
+		CloseableHttpClient client = HttpClientBuilder.create()
+				.setDefaultCredentialsProvider(provider(username, password)).useSystemProperties().build();
+		HttpComponentsClientHttpRequestFactoryDigestAuth requestFactory = new HttpComponentsClientHttpRequestFactoryDigestAuth();
+		requestFactory.setHttpClient(client);
+		requestFactory.setHost(host);
+		requestFactory.setRealmName(realmName);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
 
+		// Add converters for multi-part form submission
+		FormHttpMessageConverter formConverter = new FormHttpMessageConverter();
+		formConverter.setCharset(Charset.forName("UTF8"));
+		restTemplate.getMessageConverters().add(formConverter);
+		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		restTemplate.getInterceptors().add(0, new AWSHeaderInterceptor());
+		
+		for (ClientHttpRequestInterceptor interceptor: restTemplate.getInterceptors()) {
+		  logger.info("interceptor " + interceptor.getClass().toString());
+		}
 
-  public static RestTemplate getRestTemplate(String hostname, int port, String scheme,
-      String realmName, String username, String password, HttpHost proxy, boolean useAwsFilter) {
-    HttpHost host = new HttpHost(hostname, port, scheme);
+		return restTemplate;
+	}
 
-
-    HttpClientBuilder clientBuilder = HttpClientBuilder.create()
-        .setDefaultCredentialsProvider(provider(username, password)).useSystemProperties();
-
-    if (proxy != null) {
-      clientBuilder.setProxy(proxy);
-    }
-    CloseableHttpClient client = clientBuilder.build();
-    HttpComponentsClientHttpRequestFactoryDigestAuth requestFactory =
-        new HttpComponentsClientHttpRequestFactoryDigestAuth();
-    requestFactory.setHttpClient(client);
-    requestFactory.setHost(host);
-    requestFactory.setRealmName(realmName);
-
-    RestTemplate restTemplate = new RestTemplate(requestFactory);
-    // Add converters for multi-part form submission
-    FormHttpMessageConverter formConverter = new FormHttpMessageConverter();
-    formConverter.setCharset(Charset.forName("UTF8"));
-    restTemplate.getMessageConverters().add(formConverter);
-    restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-    if (useAwsFilter) {
-      restTemplate.getInterceptors().add(0, new AWSHeaderInterceptor());
-    }
-
-    for (ClientHttpRequestInterceptor interceptor : restTemplate.getInterceptors()) {
-      logger.info("interceptor " + interceptor.getClass().toString());
-    }
-
-    return restTemplate;
-  }
-
-  private static CredentialsProvider provider(String username, String password) {
-    CredentialsProvider provider = new BasicCredentialsProvider();
-    UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
-    provider.setCredentials(AuthScope.ANY, credentials);
-    return provider;
-  }
+	private static CredentialsProvider provider(String username, String password) {
+		CredentialsProvider provider = new BasicCredentialsProvider();
+		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+		provider.setCredentials(AuthScope.ANY, credentials);
+		return provider;
+	}
 }
